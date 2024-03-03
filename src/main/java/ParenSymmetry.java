f6f3cb77765067f70f8827292ff9bd9fbc499a47
LydiaStoneKonstanski <yourname@example.com>
2024-03-03 18:44:16 -0500

    Problem with loading the main pkg

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -5,7 +5,17 @@ public class ParenSymmetry {
 
     private Boolean isBalanced(String s) {
         // implement this method
-        return null;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) { char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--; }
+        }
+        if (count == 0) {
+            return true;
+            }
+        else return false;
     }
 
     private void checkFile(String filename) {
